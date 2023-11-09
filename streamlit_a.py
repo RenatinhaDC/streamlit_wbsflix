8be4ffe2d708ec201d4e10390cd59fee2ddb4ff6
RenatinhaDC <143716639+RenatinhaDC@users.noreply.github.com>
2023-11-09 10:20:55 +0100

    Add files via upload

---
+++ streamlit_a.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("thhis is our amaing title")
+# create a new repository on git hub and need to be public
+#apload the .py file (uploading the file manualy and choose the py file on anaconda
